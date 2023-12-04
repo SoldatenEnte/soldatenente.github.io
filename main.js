29e471985668315040a6c0d29118cb7b0df932b3
SoldatenEnte <ostehof.weertzen@gmail.com>
2023-12-04 21:29:45 +0100

    new lock

--- main.js
+++ main.js
@@ -2,4 +2,4 @@ import { Application } from "@splinetool/runtime";
 
 const canvas = document.getElementById("canvas3d");
 const app = new Application(canvas);
-app.load("https://prod.spline.design/QiikHAOFSJYm7xwa/scene.splinecode");
+app.load("https://prod.spline.design/Ep4TGhpwkc-U3k9Z/scene.splinecode");
