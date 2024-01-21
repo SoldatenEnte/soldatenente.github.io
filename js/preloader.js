440ac2295b04386edad4432e76edaab04e5bcf34
SoldatenEnte <ostehof.weertzen@gmail.com>
2024-01-21 19:37:16 +0100

    Donnerbucht Update

--- js/preloader.js
+++ js/preloader.js
@@ -1,71 +1,71 @@
-function startLoader() {
-  let counterElement = document.querySelector(".counter");
-  let curValue = 0;
+// function startLoader() {
+//   let counterElement = document.querySelector(".counter");
+//   let curValue = 0;
 
-  function updateCounter() {
-    if (curValue === 100) return;
+//   function updateCounter() {
+//     if (curValue === 100) return;
 
-    curValue += Math.floor(Math.random() * 3) + 2;
+//     curValue += Math.floor(Math.random() * 3) + 2;
 
-    if (curValue > 100) curValue = 100;
+//     if (curValue > 100) curValue = 100;
 
-    counterElement.textContent = curValue;
+//     counterElement.textContent = curValue;
 
-    const rotation = Math.sin((curValue / 100) * Math.PI * 4) * 25;
-    gsap.to(".preloader_icon", {
-      x: (curValue / 70) * window.innerWidth,
-      rotation: rotation,
-    });
+//     const rotation = Math.sin((curValue / 100) * Math.PI * 4) * 25;
+//     gsap.to(".preloader_icon", {
+//       x: (curValue / 70) * window.innerWidth,
+//       rotation: rotation,
+//     });
 
-    setTimeout(updateCounter, Math.floor(Math.random() * 200));
-    window.scrollTo(0, document.body.scrollHeight);
-  }
+//     setTimeout(updateCounter, Math.floor(Math.random() * 200));
+//     window.scrollTo(0, document.body.scrollHeight);
+//   }
 
-  updateCounter();
-}
+//   updateCounter();
+// }
 
-startLoader();
+// startLoader();
 
-// HIDE COUNTER
-gsap.to(".counter", 0.25, {
-  delay: 4,
-  opacity: 0,
-  onComplete: function () {
-    window.scrollTo(0, 0);
-  },
-});
+// // HIDE COUNTER
+// gsap.to(".counter", 0.25, {
+//   delay: 4,
+//   opacity: 0,
+//   onComplete: function () {
+//     window.scrollTo(0, 0);
+//   },
+// });
 
-gsap.to(".bar-1", 3.5, {
-  delay: 3,
-  height: 0,
-  ease: "power4.inOut",
-});
+// gsap.to(".bar-1", 3.5, {
+//   delay: 3,
+//   height: 0,
+//   ease: "power4.inOut",
+// });
 
-gsap.to(".bar-2", 3.75, {
-  delay: 3,
-  height: 0,
-  ease: "power4.inOut",
-});
+// gsap.to(".bar-2", 3.75, {
+//   delay: 3,
+//   height: 0,
+//   ease: "power4.inOut",
+// });
 
-gsap.to(".bar-3", 4, {
-  delay: 3,
-  height: 0,
-  ease: "power4.inOut",
-});
+// gsap.to(".bar-3", 4, {
+//   delay: 3,
+//   height: 0,
+//   ease: "power4.inOut",
+// });
 
-gsap.from(".h1", 1.5, {
-  delay: 4,
-  y: 700,
-  stagger: {
-    amount: 0.5,
-  },
-  ease: "power4.inOut",
-  onComplete: function () {
-    ["overlay", "counter", "preloader_icon"].forEach((className) => {
-      const element = document.querySelector(`.${className}`);
-      if (element) {
-        element.parentNode.removeChild(element);
-      }
-    });
-  },
-});
+// gsap.from(".h1", 1.5, {
+//   delay: 4,
+//   y: 700,
+//   stagger: {
+//     amount: 0.5,
+//   },
+//   ease: "power4.inOut",
+//   onComplete: function () {
+//     ["overlay", "counter", "preloader_icon"].forEach((className) => {
+//       const element = document.querySelector(`.${className}`);
+//       if (element) {
+//         element.parentNode.removeChild(element);
+//       }
+//     });
+//   },
+// });
