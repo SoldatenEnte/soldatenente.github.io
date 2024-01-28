08e78646b677cce3313ca88755dfe87f93a7788a
SoldatenEnte <ostehof.weertzen@gmail.com>
2024-01-28 20:44:00 +0100

    Lokal Fix

--- js/lightmode.js
+++ js/lightmode.js
@@ -13,21 +13,21 @@ modeToggle.addEventListener("click", function () {
 
   if (root.classList.contains("light-mode")) {
     root.classList.remove("light-mode");
-    if (logo) logo.src = "files/logo_light.svg";
-    if (logo_2) logo_2.src = "files/logo_light.svg";
-    if (ship) ship.src = "files/icons/ship_light.svg";
-    if (impressum) impressum.src = "files/icons/impressum.svg";
-    if (shipbutton) shipbutton.src = "files/icons/ship_button.svg";
-    if (light) light.src = "files/icons/light.svg";
-    if (radiospot) radiospot.src = "files/icons/radio-light.svg";
+    if (logo) logo.src = "./files/logo_light.svg";
+    if (logo_2) logo_2.src = "./files/logo_light.svg";
+    if (ship) ship.src = "./files/icons/ship_light.svg";
+    if (impressum) impressum.src = "./files/icons/impressum.svg";
+    if (shipbutton) shipbutton.src = "./files/icons/ship_button.svg";
+    if (light) light.src = "./files/icons/light.svg";
+    if (radiospot) radiospot.src = "./files/icons/radio_light.svg";
   } else {
     root.classList.add("light-mode");
-    if (logo) logo.src = "files/logo_dark.svg";
-    if (logo_2) logo_2.src = "files/logo_dark.svg";
-    if (ship) ship.src = "files/icons/ship_dark.svg";
-    if (impressum) impressum.src = "files/icons/impressum_dark.svg";
-    if (shipbutton) shipbutton.src = "files/icons/ship_button_dark.svg";
-    if (light) light.src = "files/icons/dark.svg";
-    if (radiospot) radiospot.src = "files/icons/radio-dark.svg";
+    if (logo) logo.src = "./files/logo_dark.svg";
+    if (logo_2) logo_2.src = "./files/logo_dark.svg";
+    if (ship) ship.src = "./files/icons/ship_dark.svg";
+    if (impressum) impressum.src = "./files/icons/impressum_dark.svg";
+    if (shipbutton) shipbutton.src = "./files/icons/ship_button_dark.svg";
+    if (light) light.src = "./files/icons/dark.svg";
+    if (radiospot) radiospot.src = "./files/icons/radio_dark.svg";
   }
 });
