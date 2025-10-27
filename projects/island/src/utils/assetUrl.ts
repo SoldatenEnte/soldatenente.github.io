75ed8fbd16bb358c8d34c56ba57655ba2279c405
SoldatenEnte <ostehof.weertzen@gmail.com>
2025-10-28 00:52:46 +0100

    Fixed Island

--- projects/island/src/utils/assetUrl.ts
+++
@@ -1,7 +0,0 @@
-// src/utils/assetUrl.ts
-export const asset = (path: string) => {
-    // Remove leading slash from the path if it exists,
-    // as BASE_URL already has a trailing slash.
-    const cleanPath = path.startsWith('/') ? path.slice(1) : path;
-    return `${import.meta.env.BASE_URL}${cleanPath}`;
-};
