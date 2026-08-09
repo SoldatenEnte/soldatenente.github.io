6d822697eed47b883f922ef762dc9820245a83c9
Aaron <ostehof.weertzen@gmail.com>
2026-08-09 16:13:57 +0200

    Update Lerntrainer clean MVP

---
+++ projects/lerntrainer/sw.js
@@ -0,0 +1,16 @@
+const CACHE = 'lerntrainer-v1';
+
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', (event) => {
+  event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key)))).then(() => self.clients.claim()));
+});
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET' || new URL(event.request.url).origin !== self.location.origin) return;
+  event.respondWith(fetch(event.request).then((response) => {
+    if (response.ok) {
+      const copy = response.clone();
+      void caches.open(CACHE).then((cache) => cache.put(event.request, copy));
+    }
+    return response;
+  }).catch(() => caches.match(event.request).then((cached) => cached || caches.match('./index.html'))));
+});
